e96da25f2f21fcad1e4861211d1fa507564512b2
alekskojem <alekskojem@gmail.com>
2019-02-02 14:07:32 +0200

    Create Details.js file

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Product from './'
+export default class Product extends Component {
+	render() {
+		return (
+          <div>
+             <Product>Hello from Product</Product>
+          </div>
+			)
+	}
+}
