ce8849b8bd64c895009c1796469d5c2865e24e72
alekskojem <alekskojem@gmail.com>
2019-02-03 22:53:48 +0200

    Add react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Switch, Route} from 'react-router-dom';
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,19 +7,22 @@ import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
-import Default from "./components/Default";
 import Contacts from "./components/Contacts";
+import Default from "./components/Default";
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-      <Navbar />
-      <ProductList />
-      <Details />
-      <Cart />
-      <Default />
-      <Contacts />
+        <Navbar />
+        <Switch>
+          <Route path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/contacts" component={Contacts} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,7 +4,7 @@ export default class Product extends Component {
 	render() {
 		return (
           <div>
-             <h3>Aur Contacts</h3>
+             <h3>Our Contacts</h3>
           </div>
 			);
 	}
