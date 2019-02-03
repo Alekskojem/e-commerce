cc4b0fd1abfd4992fa7a04cdfdbfb3a562871925
alekskojem <alekskojem@gmail.com>
2019-02-04 01:12:35 +0200

    Create button container for My Cart button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import {Link} from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
+import {ButtonContainer} from "./Button";
 
 export default class Navbar extends Component {
 	render() {
 		return (
-          <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+          <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
            {/*
            https://www.iconfinder.com/icons/1243689/call_phone_icon
            Creative Commons (Attribution 3.0 Unported);
@@ -21,12 +23,33 @@ export default class Navbar extends Component {
              </li>
            </ul>
            <Link to="/cart" className="ml-auto">
-             <button>
+             <ButtonContainer>
+             <span className="mr-2">
                 <i className="fas fa-cart-plus" />
+             </span>
                 My cart
-             </button>
+             </ButtonContainer>
            </Link>
           </nav>
           );
 	}
 }
+const NavWrapper = styled.nav`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  color:var(--lightBlue);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor:pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition:all 0.5s ease-in-out;
+  &:hover{
+  	background:var(--lightBlue);
+  	color:var(--mainBlue);
+  }
+  &:focus {
+  	outline: none;
+  }
+ `;
