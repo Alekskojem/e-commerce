ae46d468dfadd412537620520e112f0525e10962
alekskojem <alekskojem@gmail.com>
2019-02-03 23:49:17 +0200

    Add link PRoducts to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,6 +13,13 @@ export default class Navbar extends Component {
            <Link to="/">
              <img src={logo} alt="store" className="navbar-brand" />
            </Link>
+           <ul className="navbar-nav align-items-center">
+             <li className="nav-item ml-5">
+                <Link to="/" className="nav-link">
+                products
+                </Link>
+             </li>
+           </ul>
           </nav>
           );
 	}
