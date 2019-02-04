6ec18292baba57d1804098e2f5fe09e08ab920ce
alekskojem <alekskojem@gmail.com>
2019-02-04 23:14:28 +0200

    Add blue color to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import {ButtonContainer} from "./Button";
 export default class Navbar extends Component {
 	render() {
 		return (
-          <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+          <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
            {/*
            https://www.iconfinder.com/icons/1243689/call_phone_icon
            Creative Commons (Attribution 3.0 Unported);
@@ -30,26 +30,16 @@ export default class Navbar extends Component {
                 My cart
              </ButtonContainer>
            </Link>
-          </nav>
+          </NavWrapper>
           );
 	}
 }
 const NavWrapper = styled.nav`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  color:var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor:pointer;
-  margin: 0.2rem 0.5rem 0.2rem 0;
-  transition:all 0.5s ease-in-out;
-  &:hover{
-  	background:var(--lightBlue);
-  	color:var(--mainBlue);
+  background: var(--mainBlue);
+  .nav-link{
+    colr:var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize !important;
   }
-  &:focus {
-  	outline: none;
-  }
+  
  `;
