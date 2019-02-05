1886380dacada401fd6e22fe210fb53b85296021
alekskojem <alekskojem@gmail.com>
2019-02-05 23:48:52 +0200

    Add procuct to product list

--- src/components/context.js
+++ src/components/context.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react"
+import {storeProducts, detailProduct} from "./data";
+const ProductContext = React.createContext();
+//Provider
+//Consumer
 
-
-export default class context extends Component {
-	 render() {
-		
-		return (
-          <div>
-    
-
-          </div>
+class ProductProvider extends Component {
+	 state ={
+    products:storeProducts,
+    detailProduct:detailProduct
+   }
+   handleDetail = () =>{
+     console.log("hello from detail");
+   }
+   addToCart = () =>{
+   console.log("hello from add to cart"); 
+   }
+   render() {
+		 return (
+          <ProductContext.Provider value={{
+          ...this.state,
+          handleDetail:this.handleDetail,
+          addToCart:this.addToCart,
+          }}>
+            {this.props.children}
+          </ProductContext.Provider>
          
 		);
 	}
 }
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import {BrowserRouter as Router} from "react-router-dom";
-import * as serviceWorker from './serviceWorker';
+import {ProductProvider} from "./context";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
- <Router>
+<ProductProvider>
+<Router>
   <App />
- </Router>,
+</Router>
+</ProductProvider>,
  document.getElementById("root")
  );
 
