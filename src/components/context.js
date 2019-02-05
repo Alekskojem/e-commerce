805510478b248fb00416150728e640e5ed572657
alekskojem <alekskojem@gmail.com>
2019-02-05 22:30:43 +0200

    Add context.js file

---
+++ src/components/context.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+
+export default class context extends Component {
+	 render() {
+		
+		return (
+          <div>
+    
+
+          </div>
+         
+		);
+	}
+}
