29b1c6e55fe9075392166f7adbb4360ac9e57676
alekskojem <alekskojem@gmail.com>
2019-02-15 00:02:43 +0200

    Add modal container

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,9 +6,21 @@ import {Link} from "react-router-dom";
 export default class Modal extends Component {
 	render() {
 		return (
-			<div>
-			<h1>hello from model</h1>
-			</div>
-		)
+			<ProductConsumer>
+			{(value) =>{
+				const {modalOpen,closeModal} = value;
+                const{img, title, price} = value.modalProduct;
+
+                if(!modalOpen){
+                	return null;
+                }
+				else{
+                <ModalContainer>
+
+				}
+
+			}}
+			</ProductConsumer>
+		);
 	}
 }
