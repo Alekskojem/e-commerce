081aeacc7b3f7d4bcf5e2894f59060b9753bed99
alekskojem <alekskojem@gmail.com>
2019-02-15 00:33:46 +0200

    Add buttons on form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -22,6 +22,19 @@ export default class Modal extends Component {
                     <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
                      <h5>item added to the cart</h5>
                      <img src={img} className="img-fluid" alt="product" />
+                     <h5>{title}</h5>
+                     <h5 className="text-muted">price : $ {price}
+                     </h5>
+                     <Link to='/'>
+                     <ButtonContainer onClick={()=>closeModal()}>
+                     continue shopping
+                     </ButtonContainer>
+                     </Link>
+                     <Link to='/'>
+                     <ButtonContainer onClick={()=>closeModal()}>
+                     go to cart
+                     </ButtonContainer>
+                     </Link>
                     </div>
                    </div>
                   </div>
