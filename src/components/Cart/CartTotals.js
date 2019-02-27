29286327f9a933be6e623660a113a029e3b71487
alekskojem <alekskojem@gmail.com>
2019-02-27 23:06:46 +0200

    Create CartTotals.js file

---
+++ src/components/Cart/CartTotals.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CartTotals() {
+  const{ cartSubtotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <React.Fragment>
+    <div className="container">
+    </React.Fragment>
+
+    </div>
+
+  	)
+}
