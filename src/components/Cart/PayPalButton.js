a35ed495b8c3a603bec3fb79f533cecc0be5bdfe
alekskojem <alekskojem@gmail.com>
2019-06-12 00:17:30 +0300

    PayPallButton.js settings config

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -6,8 +6,11 @@ export default class MyApp extends React.Component {
         const onSuccess = (payment) => {
             // Congratulation, it came here means everything's fine!
             		console.log("The payment was succeeded!", payment);
+            		this.props.clearCart();
+            		this.props.history.push('/');            			// body...
+            		};
             		// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
-        }
+        };
  
         const onCancel = (data) => {
             // User pressed "cancel" or close Paypal's popup!
