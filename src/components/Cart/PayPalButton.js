0e5ab42e47ba1c7535dc8e0f8f1e4d4bfea30683
alekskojem <alekskojem@gmail.com>
2019-06-12 00:29:14 +0300

    PayPal total props

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -7,16 +7,14 @@ export default class MyApp extends React.Component {
             // Congratulation, it came here means everything's fine!
             		console.log("The payment was succeeded!", payment);
             		this.props.clearCart();
-            		this.props.history.push('/');            			// body...
-            		};
-            		// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
-        };
+            		this.props.history.push('/');            			
+        }
  
         const onCancel = (data) => {
             // User pressed "cancel" or close Paypal's popup!
             console.log('The payment was cancelled!', data);
             // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
-        }
+        };
  
         const onError = (err) => {
             // The main Paypal's script cannot be loaded or somethings block the loading of that script!
@@ -27,7 +25,7 @@ export default class MyApp extends React.Component {
  
         let env = 'sandbox'; // you can set here to 'production' for production
         let currency = 'USD'; // or you can set this value from your props or state
-        let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
+        //let total = 1; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
@@ -42,7 +40,15 @@ export default class MyApp extends React.Component {
  
         // NB. You can also have many Paypal express checkout buttons on page, just pass in the correct amount and they will work!
         return (
-            <PaypalExpressBtn env={env} client={client} currency={currency} total={total} onError={onError} onSuccess={onSuccess} onCancel={onCancel} />
+            <PaypalExpressBtn
+             env={env} 
+             client={client} 
+             currency={currency} 
+             total={this.props.totalAmount} 
+             onError={onError} 
+             onSuccess={onSuccess} 
+             onCancel={onCancel} 
+             />
         );
     }
 }
